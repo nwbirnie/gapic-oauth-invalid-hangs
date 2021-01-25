c94e5fd703fe8b5360ac9bbfd8edb518c66e4d74
Nick Birnie <nbirnie@google.com>
2021-01-25 17:53:37 +0000

    Add repro steps for https://github.com/googleapis/gax-java/issues/965

---
+++ src/main/java/Issue.java
@@ -0,0 +1,45 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import com.google.ads.googleads.v6.services.GoogleAdsServiceClient;
+import com.google.ads.googleads.v6.services.GoogleAdsServiceSettings;
+import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
+import com.google.auth.oauth2.UserCredentials;
+import java.io.IOException;
+
+public class Issue {
+
+  public static void main(String[] args) throws IOException {
+    UserCredentials credentials =
+        UserCredentials.newBuilder()
+            .setClientId("")
+            .setClientSecret("")
+            .setRefreshToken("this does not exist")
+            .build();
+
+    GoogleAdsServiceSettings settings =
+        GoogleAdsServiceSettings.newBuilder()
+            .setTransportChannelProvider(InstantiatingGrpcChannelProvider.newBuilder().build())
+            .setCredentialsProvider(() -> credentials)
+            .build();
+
+    GoogleAdsServiceClient client = GoogleAdsServiceClient.create(settings);
+
+    System.out.println("issuing request");
+
+    client.search("123", "select foo");
+
+    System.out.println("never reached");
+  }
+}
